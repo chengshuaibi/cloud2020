4c8e7fb5ae470d1e081043fd745174ad00070770
chengwentao <445849281@qq.com>
2021-12-08 20:08:25 +0800

    重新部署项目

--- cLoud-consumer-order80/src/main/java/springcloud/OrderMain80.java
+++ cLoud-consumer-order80/src/main/java/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package springcloud;
 
+import configure.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="",configuration = MyselfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cLoud-consumer-order80/src/main/java/springcloud/controller/OrderController.java
+++ cLoud-consumer-order80/src/main/java/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,25 @@ import com.atguigu.springcloud.entities.Payment;
 @RestController
 @Slf4j
 public class OrderController {
-    private final static String  url="http://CLOUD-PAYMENT-SERVICE";
+    private final static String  payment_url="http://CLOUD-PAYMENT-SERVICE";
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
     public CommentResult<Payment> create(Payment payment){
-        return restTemplate.postForObject(url+"/payment/create",payment,CommentResult.class);
+        return restTemplate.postForObject(payment_url+"/payment/create",payment,CommentResult.class);
     }
     @GetMapping("/consumer/payment/get/{id}")
     public CommentResult<Payment> getPayment(@PathVariable("id")Long id){
-        return restTemplate.getForObject(url+"/payment/get/"+id,CommentResult.class);
+        return restTemplate.getForObject(payment_url+"/payment/get/"+id,CommentResult.class);
     }
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommentResult<Payment> payment2(@PathVariable("id") int id){
+        ResponseEntity<CommentResult> forEntity = restTemplate.getForEntity(payment_url + "/payment/get/" + id, CommentResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()){
+            return forEntity.getBody();
+        }else{
+            return new CommentResult<Payment>(444,"服务报错");
+        }
+    };
 }
